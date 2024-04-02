c3cf7d5f53cea01f96a56b32e062767521bae3a7
Moloy Sikka <148451746+MoloySikka@users.noreply.github.com>
2024-04-02 20:32:24 +0530

    Update character-line.py 2/4/24

--- character-line.py
+++ character-line.py
@@ -1,4 +1,5 @@
 # 19/3/24
+# AIM: WAP to create a line with a given character
 
 char = input("Enter the character you want to make the line with: ")
 length = int(input("Enter length of the line: "))
