08fa934ca3e277b02b2ffa4126552922b647e3b0
Moloy Sikka <148451746+MoloySikka@users.noreply.github.com>
2024-08-25 21:50:25 +0530

    Update 25/8/24

---
+++ 31-stacks.py
@@ -0,0 +1,44 @@
+# 21/8/24
+# AIM: To demonstrate the basic operations of a stack, implemented using a # Python list.
+
+mystack = []
+
+
+def fillStack():
+    for i in range(5):
+        a = int(input('Enter number: '))
+        mystack.append(a)
+
+
+fillStack()
+
+
+def push():
+    a = int(input('Enter number to be pushed: '))
+    mystack.append(a)
+    print('\nUpdated stack is: ', mystack)
+
+
+def pop():
+    mystack.pop()
+    print('\nUpdated stack is: ', mystack)
+
+
+def peek():
+    print(mystack[-1])
+
+
+while True:
+    text = "\nWould you like to: \npush (1) \npop  (2) \npeek (3) \nexit (4): "
+    ans = int(input(text))
+    if ans == 1:
+        push()
+    elif ans == 2:
+        pop()
+    elif ans == 3:
+        peek()
+    elif ans == 4:
+        print('Thank you for using the program')
+        break
+    else:
+        print('Please enter respective number.')
